a0538160aaadcc4ace5af8aaaef14649f387843f
shahram.moshiri@yahoo.com <shahram.moshiri@yahoo.com>
2023-07-12 15:38:09 +0330

    Change Folder Structure

---
+++ src/features/humanresource/personnel.js
@@ -0,0 +1,24 @@
+import React  from "react";
+import { Grid } from "../../components";
+import {useState } from "react";
+
+const Personnel = () =>{  
+        const [gridOptions , setGridOptions ] =useState ( {
+                url  : 'http://localhost:3005/users' ,
+                columnItem: [
+                        {name : 'id' ,  title : 'شناسه' ,isEditable : false , type: Number , isFilterable : false , isSortable: true   } , 
+                        {name : 'firstName' , title : 'نام' ,isEditable : false , type:String , isFilterable : false , isSortable: true} ,
+                        {name : 'lastName' , title : 'نام خانوادگی' ,isEditable : false , String, isFilterable : false , isSortable: true}
+                ] , 
+                height: 500 ,
+                width: 800 , 
+                numberRows : 10 ,
+                allowInsert: false,
+        } );
+
+        return (
+                <Grid options={gridOptions}> </Grid>
+        );
+};
+
+export default Personnel;
